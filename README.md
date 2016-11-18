7e85b628da944d9fc470fafd09d0ce7f55e537a9
Adrien <adrien.rod@aliceadsl.fr>
2016-11-18 11:38:45 +0100

    cmake updated

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Build an executable for Windows (x86_64 processors instruction set) : debian instructions
 
+ * Compile boost : `./b2 toolset=gcc-meng target-os=windows threadapi=win32 --build-type=complete --prefix=/usr/x86_64-w64-mingw32 --layout=tagged --without-python
+`
+
  * Require package `mingw-w64`
  * Create build directory `mkdir build-win`
  * Go to it `cd build-win`
--- Toolchain-mingw-w64.cmake
+++ Toolchain-mingw-w64.cmake
@@ -1,6 +1,11 @@
 # the name of the target operating system
 SET(CMAKE_SYSTEM_NAME Windows)
 
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -static-libgcc")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static-libgcc -static-libstdc++")
+set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS  "${CMAKE_SHARED_LIBRARY_LINK_C_FLAGS} -static-libgcc -s")
+set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "${CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS} -static-libgcc -static-libstdc++ -s")
+
 # which compilers to use for C and C++
 SET(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 SET(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
