d29caa8da1cd6af0995861bfbfe5b265d19a172d
LE DILY Hugo <hledily@caribert.pedago.ipb.fr>
2017-03-10 17:18:41 +0100

    le générateur de confetti (solveur pseudo-physique)

--- src/solver/box/Freeze.cpp
+++ src/solver/box/Freeze.cpp
@@ -14,42 +14,66 @@
 #include "common.hpp"
 #include "Display.hpp"
 
-#define PHYSICS_GRAVITY_TIME        500
-#define PHYSICS_STABILIZATION_TIME  500
+#define PHYSICS_GRAVITY_TIME        100
+#define PHYSICS_STABILIZATION_TIME  1000
 #define PHYSICS_ALL_TIME			(PHYSICS_GRAVITY_TIME + PHYSICS_STABILIZATION_TIME)
 #define GRAVITY_STEP                10.
-#define BOUNCE_STEP                 10.
-#define REPEL_STEP_MULTIPLE			1.
+#define BOUNCE_STEP                 1.
+#define REPEL_STEP_MULTIPLE			5.
 
 
 using namespace std;
 
+
+void Freeze::preSolve() {
+    for (unsigned i = 0; i < _shapes.size(); ++i)
+        rotateToBestAngle(_shapes[i]);
+}
+
+
 // Principe: On balance une pièce au dessus
 void Freeze::solveBin(){
     Box box;
+    std::list<unsigned> indiceCopy;
+    indiceCopy.resize(_shapes.size());
+    iota(indiceCopy.begin(), indiceCopy.end(), 0);
+    
+    for(auto && i : _indices){
+		awayStartingPoint(_shapes[i]);
+	}
+    
+    for(list<unsigned>::iterator i = _indices.begin(); i != _indices.end(); ++i) {
 
-    for (unsigned i = 0 ; i < _shapes.size(); ++i) {
-
-        randomStartingPoint(_shapes[i]);
+        randomStartingPoint(_shapes[*i]);
+        wiggle(_shapes[*i]);
+        
 
         for(unsigned t = 0 ; t < PHYSICS_ALL_TIME ; ++t){
-            bg::envelope(_shapes[i].getMultiP(), box);
+            bg::envelope(_shapes[*i].getMultiP(), box);
 
-            if(box.max_corner().y() > Parser::getDims().y()){
-                bounceUp(_shapes[i]); // Move up + random slight rotation
+            if(box.max_corner().y() > (_binNumber+1) * Parser::getDims().y()){
+                bounceUp(_shapes[*i]); // Move up + random slight rotation
             } else if(box.min_corner().x() < 0){
-                bounceRight(_shapes[i]);
+                bounceRight(_shapes[*i]);
             } else if(box.max_corner().x() > Parser::getDims().x()){
-                bounceLeft(_shapes[i]);
+                bounceLeft(_shapes[*i]);
             } else{
-                for(unsigned j = 0 ; j < i ; ++j){
-                    if(bg::intersects(_shapes[i].getMultiP(), _shapes[j].getMultiP())){
-                        bounceShape(_shapes[i],_shapes[j]);
+                for(auto && j : indiceCopy){
+                    if(j < *i && bg::intersects(_shapes[*i].getMultiP(), _shapes[j].getMultiP())){
+                        if(rand()%3 == 0){
+							bounceUp(_shapes[*i]);
+						} else if(rand()%2 ==0){
+							bounceLeft(_shapes[*i]);
+						} else{
+							bounceRight(_shapes[*i]);
+						}
+							
+                        //bounceShape(_shapes[*i],_shapes[j]);
                     }
                 }
 
                 if(t < PHYSICS_GRAVITY_TIME){
-					gravityMoveDown(_shapes[i]);
+					gravityMoveDown(_shapes[*i]);
                 }
 
             }
@@ -58,64 +82,83 @@ void Freeze::solveBin(){
         // Checking if stabilization worked, else shape is sent to next bin
         bool stabilized = true;
         
-        bg::envelope(_shapes[i].getMultiP(), box);
-        if(box.min_corner().x() < 0 || box.min_corner().x() > Parser::getDims().x() ||
-           box.min_corner().y() < 0 || box.min_corner().y() > Parser::getDims().y()){
+        bg::envelope(_shapes[*i].getMultiP(), box);
+        if(box.min_corner().x() < 0 || box.max_corner().x() > Parser::getDims().x() ||
+           box.min_corner().y() < _binNumber * Parser::getDims().y() ||
+           box.max_corner().y() > (_binNumber+1) * Parser::getDims().y()){
 			stabilized = false;
 		   }
+		   
 		if(stabilized != false){
-			for(unsigned j = 0 ; j < i ; ++j){
-				if(bg::intersects(_shapes[i].getMultiP(), _shapes[j].getMultiP())){
+			for(auto && j : indiceCopy){
+				if(j < *i && bg::intersects(_shapes[*i].getMultiP(), _shapes[j].getMultiP())){
 					stabilized = false;
 					break;
                 }
 			}
 		}
+		
+		if(stabilized == true){
+			LOG(info) << "Packed : " << *i << endl;
+			markPacked(i);
+		}
+		
     }
-    _indices.clear();
 }
 
-void Freeze::randomStartingPoint(Shape shape){
+
+void Freeze::awayStartingPoint(Shape& shape){
+	Box box;
+	bg::envelope(shape.getMultiP(), box);
+	
+	// Translation above
+	translate<Shape>(shape, -box.min_corner().x() - Parser::getDims().x() * SPACE_COEF,
+							-box.min_corner().y());
+	
+	
+}
+
+void Freeze::randomStartingPoint(Shape& shape){
 	Box box;
 	bg::envelope(shape.getMultiP(), box);
 	
 	// Translation to bin origin
-	translate<Shape>(shape, -box.min_corner().x(),
+	translate<Shape>(shape, -box.min_corner().x() + Parser::getDims().x()/2,
 							-box.min_corner().y() + Parser::getDims().y() * _binNumber * SPACE_COEF);
-	
-	
 }
 
 
 
 
-void Freeze::gravityMoveDown(Shape shape){
+void Freeze::gravityMoveDown(Shape& shape){
     translate<Shape>(shape, 0., GRAVITY_STEP);
 }
 
-void Freeze::wiggle(Shape shape){
+void Freeze::wiggle(Shape& shape){
 	float randomX, randomY, randomAngle;
 	
-	randomX = 0.;
-	randomY = 0.;
-	randomAngle = 0.;
+	// First bin randoms can be: -1,0,1
+	// Then the shape can fly upwards
+	randomX = rand()%101 - 50.;
+	randomY = rand()%3 - 1. - (_binNumber/5.);
+	randomAngle = rand()%3 -1.;
 	
 	translate<Shape>(shape, randomX, randomY);
 	rotate<Shape>(shape, randomAngle);
 }
 
 
-void Freeze::bounceUp(Shape shape){
+void Freeze::bounceUp(Shape& shape){
     translate<Shape>(shape, 0., -BOUNCE_STEP);
     wiggle(shape);
 }
 
-void Freeze::bounceLeft(Shape shape){
+void Freeze::bounceLeft(Shape& shape){
     translate<Shape>(shape, -BOUNCE_STEP, 0.);
     wiggle(shape);
 }
 
-void Freeze::bounceRight(Shape shape){
+void Freeze::bounceRight(Shape& shape){
     translate<Shape>(shape, BOUNCE_STEP, 0.);
     wiggle(shape);
 }
@@ -129,7 +172,7 @@ void Freeze::bounceShape(Shape& shapeMove, Shape& shapeStill){
     int diffX = (boxS.max_corner().x() - boxS.min_corner().x())/2 - (boxM.max_corner().x() - boxM.min_corner().x())/2;
     int diffY = (boxS.max_corner().y() - boxS.min_corner().y())/2 - (boxM.max_corner().y() - boxM.min_corner().y())/2;
 
-    translate<Shape>(shapeMove, REPEL_STEP_MULTIPLE*diffX, REPEL_STEP_MULTIPLE*diffY);
+    translate<Shape>(shapeMove, -REPEL_STEP_MULTIPLE*diffX, -REPEL_STEP_MULTIPLE*diffY);
     
     wiggle(shapeMove);
 }
--- src/solver/box/Freeze.hpp
+++ src/solver/box/Freeze.hpp
@@ -7,14 +7,16 @@
 
 class Freeze : public Solver {
 private:
+	void preSolve() override; 
     void solveBin() override;
     
-    void randomStartingPoint(Shape shape);
-    void gravityMoveDown(Shape shape);
-    void wiggle(Shape shape);
-    void bounceUp(Shape shape);
-    void bounceLeft(Shape shape);
-    void bounceRight(Shape shape);
+    void awayStartingPoint(Shape& shape);
+    void randomStartingPoint(Shape& shape);
+    void gravityMoveDown(Shape& shape);
+    void wiggle(Shape& shape);
+    void bounceUp(Shape& shape);
+    void bounceLeft(Shape& shape);
+    void bounceRight(Shape& shape);
     void bounceShape(Shape& shapeMove, Shape& shapeStill);
     
 public:
