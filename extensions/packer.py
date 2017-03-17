d3f8cbb58529f3d84dbae9d2ecd1e98ec2d3f8fe
vrobles <vrobles@enseirb-matmeca.fr>
2017-03-17 02:25:11 +0100

    Gestion des unités (via Inkscape) OK

--- extensions/packer.py
+++ extensions/packer.py
@@ -8,33 +8,33 @@ class Packer(inkex.Effect):
     def __init__(self):
         inkex.Effect.__init__(self)
         self.OptionParser.add_option("--width",
-                        action="store", type="int", 
+                        action="store", type="string", 
                         dest="width", default=0,
-                        help="Le caca partout")
+                        help="Une option")
         self.OptionParser.add_option("--height",
-                        action="store", type="int", 
+                        action="store", type="string", 
                         dest="height", default=0,
-                        help="Le caca partout")
+                        help="Une option")
         self.OptionParser.add_option("--dup",
                         action="store", type="inkbool", 
                         dest="dup", default=0,
-                        help="Le caca partout")
+                        help="Une option")
         self.OptionParser.add_option("--buffer",
                         action="store", type="int", 
                         dest="buffer", default=0,
-                        help="Le caca partout")
+                        help="Une option")
         self.OptionParser.add_option("--display",
                         action="store", type="inkbool", 
                         dest="display", default=0,
-                        help="Le caca partout")
+                        help="Une option")
         self.OptionParser.add_option("--conf",
                         action="store", type="string", 
                         dest="conf", default="default.ce",
-                        help="Le caca partout")
+                        help="Une option")
         self.OptionParser.add_option("--tab",
                         action="store", type="string", 
                         dest="tab", default="def",
-                        help="Le caca partout")
+                        help="Une option")
         self.OptionParser.add_option("--defWidth",
                         action="store", type="inkbool", 
                         dest="defWidth", default=1,
@@ -44,12 +44,31 @@ class Packer(inkex.Effect):
                         dest="defHeight", default=1,
                         help="Use default height")
     def effect(self):
+        #Raw width/height
+        swidth = self.document.getroot().get('viewBox').split()[2] if self.options.defWidth else self.options.width
+        sheight = self.document.getroot().get('viewBox').split()[3] if self.options.defHeight else self.options.height
+        try:
+            #If raw int, do not convert
+            width = int(swidth)
+        except ValueError:
+            #If contains units, convert
+            width = int(self.unittouu(swidth))
+        try:
+            height = int(sheight)
+        except ValueError:
+            height = int(self.unittouu(sheight))
+        sys.stderr.write("Width (px) : " + str(width));
+        sys.stderr.write("Height (px) : " + str(height));
         t = []
+        #Replace inkscape parameters by computed ones
         for a in sys.argv[1:]:
-            if(not ("--width" in a and self.options.defWidth or "--height" in a and self.options.defHeight)):
-                t += [a];
+            if("--width" in a):
+                t.append("--width=" + str(width));
+            elif("--height" in a):
+                t.append("--height=" + str(height));
             else:
-                sys.stderr.write("Utilisation de la valeur par defaut");
+                t += [a];
+        #Call packer with parameters
         call(["./packer"] + t)
 
 if __name__ == '__main__':
