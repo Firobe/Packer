8ff852ee195112d8bf9306c1576793a5f7896a27
ROBLES Virgile <=>
2017-03-17 15:02:14 +0100

    Windows truc

--- extensions/packer.py
+++ extensions/packer.py
@@ -69,7 +69,7 @@ class Packer(inkex.Effect):
             else:
                 t += [a];
         #Call packer with parameters
-        call(["./packer"] + t)
+        call(["@EXE_NAME_PLUGIN@"] + t)
 
 if __name__ == '__main__':
     e = Packer()
