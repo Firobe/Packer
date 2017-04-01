e5812e281200477826b87283dcc2160312b4c721
vrobles <vrobles@enseirb-matmeca.fr>
2017-04-01 18:34:11 +0200

    Commentaires ProbaSolver

--- src/solver/ProbaSolver.cpp
+++ src/solver/ProbaSolver.cpp
@@ -14,7 +14,10 @@
 using namespace std;
 
 void ProbaSolver::preSolve() {
+	//If the user did not prohibit it, use MultilineSolver to place the shapes
+	//such that they do not intersect
     if (_initialPlacement) {
+		//Random initial order
         random_shuffle(_shapes.begin(), _shapes.end());
         MultilineSolver s(_shapes, {{"sort", 0}});
         s.solve();
@@ -23,9 +26,11 @@ void ProbaSolver::preSolve() {
     _centroids.reserve(_shapes.size());
 
     for (Shape& s : _shapes) {
+		//Cache the centroids of the shape
         _centroids.emplace_back(s.centroid());
         Box e;
         s.envelope(e);
+		//Cache the bin number of each shape
         _binNbs.emplace_back(e.min_corner().y() / (Parser::getDims().y() * SPACE_COEF));
         _binNumber = max(_binNumber, _binNbs.back());
     }
@@ -34,23 +39,28 @@ void ProbaSolver::preSolve() {
 }
 
 void ProbaSolver::solveBin() {
+	//Save the current state
     Solution current;
     genSolution(current);
     LOG(info) << "Doing random steps";
 
-    for (int i = 0 ; i < 50 ; ++i) {
-        for (unsigned step = 0 ; step < 20 ; ++step)
+    for (int i = 0 ; i < _branchSteps ; ++i) {
+		//Do random steps
+        for (int step = 0 ; step < _randomSteps ; ++step)
             nextStep();
 
+		//Evaluate the quality of the resulting solution
         Solution newS;
         genSolution(newS);
 
         if (newS.quality >= current.quality) {
+			//Restore old state if not improving
             Display::Text("Iteration " + to_string(i) + " discarded");
             applySolution(current);
             LOG(info) << ".";
         }
         else {
+			//Replace old state by new one if improving
             Display::UpdateAll();
             Display::Text("Iteration " + to_string(i) + " accepted");
             current = newS;
@@ -62,12 +72,22 @@ void ProbaSolver::solveBin() {
     _indices.clear();
 }
 
-double ProbaSolver::swagRNG() const {
+/**
+ * @brief Random number generator
+ * Uses an exponential distribution
+ */
+double ProbaSolver::RNG() const {
     static default_random_engine generator;
     static exponential_distribution<double> distribution(_amplitudeProba);
     return distribution(generator);
 }
 
+/**
+ * @brief Check that a shape at index i is in its own bin
+ * If binNb is specified, check for that bin
+ * @param i
+ * @param binNb
+ */
 bool ProbaSolver::shapeInBin(unsigned i, int binNb) const {
     if (binNb == -1)
         binNb = _binNbs[i];
@@ -81,13 +101,15 @@ bool ProbaSolver::shapeInBin(unsigned i, int binNb) const {
 }
 
 /**
- * Next iteration
+ * @brief Next iteration
+ * Moves each shape with a random translation/rotation
+ * and checks that is does not intersect with other (cancel the movement if it does)
  */
 void ProbaSolver::nextStep() {
     for (unsigned i = 0 ; i < _shapes.size() ; ++i) {
-        double angle = (1 - (rand() % 2) * 2) * swagRNG() * 100;
-        double tX = (0.9 - (rand() % 2) * 2) * swagRNG() * Parser::getDims().x() / 10,
-               tY = (0.9 - (rand() % 2) * 2) * swagRNG() * Parser::getDims().y() / 10;
+        double angle = (1 - (rand() % 2) * 2) * RNG() * 100;
+        double tX = (0.9 - (rand() % 2) * 2) * RNG() * Parser::getDims().x() / 10,
+               tY = (0.9 - (rand() % 2) * 2) * RNG() * Parser::getDims().y() / 10;
         _shapes[i].translate(-_centroids[i].x(), -_centroids[i].y());
         _shapes[i].rotate(angle);
         _shapes[i].translate(_centroids[i].x() + tX, _centroids[i].y() + tY);
@@ -105,11 +127,15 @@ void ProbaSolver::nextStep() {
             _shapes[i].rotate(-angle);
             _shapes[i].translate(_centroids[i].x() - tX, _centroids[i].y() - tY);
         }
-
         //this_thread::sleep_for(chrono::milliseconds(2));
     }
 }
 
+/**
+ * @brief Evaluates the quality of a solution
+ * Currently sums the squared distance of each shape
+ * to the origin
+ */
 double ProbaSolver::quality() const {
     double sum = 0;
 
@@ -122,6 +148,11 @@ double ProbaSolver::quality() const {
     return sum;
 }
 
+/**
+ * @brief Creates a Solution from the current state
+ * (generates every transformation Matrix)
+ * @param s Where the solution will be generated
+ */
 void ProbaSolver::genSolution(Solution& s) const {
     s.mats.clear();
     s.mats.reserve(_shapes.size());
@@ -132,6 +163,11 @@ void ProbaSolver::genSolution(Solution& s) const {
     s.quality = quality();
 }
 
+/**
+ * @brief Apply a solution to the current state
+ * (apply every transformation matrix)
+ * @param s The solution to apply
+ */
 void ProbaSolver::applySolution(Solution& s) {
     for (unsigned i = 0 ; i < _shapes.size() ; ++i) {
         _shapes[i].restore();
--- src/solver/ProbaSolver.hpp
+++ src/solver/ProbaSolver.hpp
@@ -2,6 +2,8 @@
 #define PROBASOLVER__H
 
 #define DEFAULT_AMPLITUDE (10.)
+#define DEFAULT_RANDOM (50)
+#define DEFAULT_BRANCH (50)
 
 #include "Solver.hpp"
 #include "CloseEnough.hpp"
@@ -12,23 +14,38 @@ struct Solution {
     double quality;
 };
 
+/**
+ * @brief Solver exploiting random steps
+ * @param random_steps The number of completely random steps per branch
+ * @param branch_steps The number of branches to try
+ * @param amplitude_proba An indicator of the probability of a shape doing wild movements
+ * The higher the slower
+ */
 class ProbaSolver : public Solver {
 private:
     double _amplitudeProba;
     bool _initialPlacement;
     std::vector<Point> _centroids;
     std::vector<unsigned> _binNbs;
+	int _randomSteps;
+	int _branchSteps;
     void preSolve() override;
     void solveBin() override;
 
     void nextStep();
-    double swagRNG() const;
+    double RNG() const;
     bool shapeInBin(unsigned, int = -1) const;
     void applySolution(Solution& s);
     void genSolution(Solution& s) const;
     double quality() const;
 public:
     ProbaSolver(Layout& v, const std::vector<Parameter>& p) : Solver(v, p) {
+        if (!getParameter(p, "random_steps", _randomSteps))
+            _randomSteps = DEFAULT_RANDOM;
+
+        if (!getParameter(p, "branch_steps", _branchSteps))
+            _branchSteps = DEFAULT_BRANCH;
+
         if (!getParameter(p, "amplitude_proba", _amplitudeProba))
             _amplitudeProba = DEFAULT_AMPLITUDE;
 
